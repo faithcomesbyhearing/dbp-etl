25d64ac991d10ccf1a9791876ef1df1aebbd1472
Gary N Griswold <gary@shortsands.com>
2019-09-24 13:14:58 -0400

    Added downloadBucketListing

---
+++ analysis/py/DownloadBucketList.py
@@ -0,0 +1,46 @@
+# DownloadBucketList
+#
+# This program lists all of the keys in a bucket into a text file
+# It stores this in the metadata directory where it can be used
+#
+
+import boto3
+import io
+import os
+import sys
+
+BUCKET_NAME = "dbp-vid"
+#BUCKET_NAME = "dbp-prod"
+
+filename = "new_%s.txt" % (BUCKET_NAME.replace("-", "_"))
+pathname = "%s/FCBH/bucket_data/%s" % (os.environ['HOME'], filename)
+print(pathname)
+
+out = io.open(pathname, mode="w", encoding="utf-8")
+
+session = boto3.Session(profile_name='FCBH_Gary')
+client = session.client('s3')
+
+request = { 'Bucket':BUCKET_NAME, 'MaxKeys':1000 }
+# Bucket, Delimiter, EncodingType, Market, MaxKeys, Prefix
+
+hasMore = True
+while hasMore:
+	response = client.list_objects_v2(**request)
+	hasMore = response['IsTruncated']
+	contents = response['Contents']
+	for item in contents:
+		key = item['Key']
+		size = item['Size']
+		if (size > 0):
+			try:
+				#print(key)
+				out.write("%s\n" % (key))
+			except Exception as err:
+				print("Could not write key %s" % (err))
+
+	if hasMore:
+		request['ContinuationToken'] = response['NextContinuationToken']
+
+out.close()
+
--- analysis/run_all.sh
+++ analysis/run_all.sh
@@ -1,34 +1,48 @@
 #!/bin/sh -v -e
 
 python3 py/BucketListingTable.py
+# 1109357 9/24
 
 python3 py/BiblesTable.py
+# 1397 9/24
 
 python3 py/BibleTranslationsTable.py
+# 1397 9/24
 
 python3 py/BibleFilesetsTable.py
+# 3728 9/24
 
 python3 py/BibleFilesetConnectionsTable.py
+# 3779 9/24
 
 python3 py/BibleFilesetCopyrightsTable.py
+# 3728 9/24
 
 python3 py/BibleFilesetCopyrightOrganizationsTable.py
+# 3728 9/24
 
 python3 py/BibleFilesetTagsTable.py
+# 10704 9/24
 
 python3 py/AccessGroupFilesetsTable.py
+# 3686 9/24
 
 # to be written possibly use info.json files
 # python3 py/BibleBooksTable.py
 # insert into valid_dbp.bible_books select * from dbp.bible_books where bible_id in (select id from valid_dbp.bibles);
+# 42925 9/24
 
 python3 py/BibleFilesTable.py
+# 1086357 9/24
 
 python3 py/BibleFileTagsTable.py
+# 0 9/24
 
 python3 py/BibleFileVideoResolutionsTable.py
+# 0 9/24
 
 python3 py/BibleFileVideoTransportStreamTable.py
+# 0 9/24
 
 
 
