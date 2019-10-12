e90fc1f6c861d549f09901e67d45c6c273899f85
Gary N Griswold <gary@shortsands.com>
2019-10-12 01:24:25 -0400

    BibleFilesTable rewitten to use new program structure.

--- analysis/py/BibleFilesTable.py
+++ analysis/py/BibleFilesTable.py
@@ -22,86 +22,91 @@ from SQLUtility import *
 
 class BibleFilesTable:
 
-	def __init__(self, config):
+	def __init__(self, config, db):
 		self.config = config
+		self.db = db
+
+
+	def process(self):
+		self.readAll()
+
+		results = []
+		uniqueSet = set()
+
+		for fileset in self.bucketList:
+			fileName = fileset[0]
+			hashId = fileset[1]
+			bookId = fileset[2]
+			chapterStart = fileset[3]
+			if chapterStart == "End":
+				if bookId == "MAT":
+					chapterStart = "28"
+					verseStart = "21"
+					verseEnd = "21"
+				elif bookId == "MRK":
+					chapterStart = "16"
+					verseStart = "21"
+					verseEnd = "21"
+				elif bookId == "LUK":
+					chapterStart = "24"
+					verseStart = "54"
+					verseEnd = "54"
+				elif bookId == "JHN":
+					chapterStart = "21"
+					verseStart = "26"
+					verseEnd = "26"
+				else:
+					chapterStart = "0"
+					verseStart = None
+					verseEnd = None
+			chapterEnd = None
+			verseStart = fileset[4]
+			if verseStart != None:
+				if "b" in verseStart:
+					verseStart = verseStart.replace("b", "")
+			verseEnd = fileset[5]
+			if verseEnd != None:
+				if "r" in verseEnd:
+					verseEnd = verseEnd.replace("r", "")
+				if "a" in verseEnd:
+					verseEnd = verseEnd.replace("a", "")
+			fileSize = None
+			duration = None
+
+			key = "%s-%s-%s-%s" % (hashId, bookId, chapterStart, verseStart)
+			if key in uniqueSet:
+				print("WARNING: dropping duplicate key ", fileset)
+			elif hashId not in self.hashIdSet:
+				print("WARNING: dropping hash_id mismatch ", fileset)
+			elif chapterStart == "0" and fileName[-5:] == ".html":
+				# This drop is probably not correct for production, correct feedback from App devs
+				print("WARNING: dropping zero chapter html files")
+			else:
+				uniqueSet.add(key)
+				results.append((hashId, bookId, chapterStart, chapterEnd, verseStart, verseEnd, fileName, fileSize, duration))
+
+		print("num records to insert %d" % (len(results)))
+		self.db.executeBatch("INSERT INTO bible_files (hash_id, book_id, chapter_start, chapter_end, verse_start, verse_end, file_name, file_size, duration) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)", results)
 
 
 	def readAll(self):
-		validDB = SQLUtility(self.config.database_host, self.config.database_port,
-			self.config.database_user, self.config.database_output_db_name)
 		sql = ("SELECT file_name, hash_id, book_id, chapter_start, verse_start, verse_end"
 			+ " FROM bucket_listing"
 			+ " WHERE book_id IS NOT NULL AND chapter_start is NOT NULL"
 			+ " AND book_id IN (SELECT id FROM books)"
 			+ " ORDER by hash_id, book_id, chapter_start, verse_start, length(file_name) DESC, file_name")
-		self.bucketList = validDB.select(sql, None)
+		self.bucketList = self.db.select(sql, None)
 		print("num %d files rows in bucket_listing" % (len(self.bucketList)))
-		self.hashIdSet = validDB.selectSet("SELECT hash_id FROM bible_filesets", None)
-		validDB.close()
+		self.hashIdSet = self.db.selectSet("SELECT hash_id FROM bible_filesets", None)
 
 
 config = Config()
-files = BibleFilesTable(config) 
-files.readAll()
+db = SQLUtility(config.database_host, config.database_port,
+				config.database_user, config.database_output_db_name)
+files = BibleFilesTable(config, db)
+files.process()
+db.close() 
 
-results = []
-uniqueSet = set()
-
-for fileset in files.bucketList:
-	fileName = fileset[0]
-	hashId = fileset[1]
-	bookId = fileset[2]
-	chapterStart = fileset[3]
-	if chapterStart == "End":
-		if bookId == "MAT":
-			chapterStart = "28"
-			verseStart = "21"
-			verseEnd = "21"
-		elif bookId == "MRK":
-			chapterStart = "16"
-			verseStart = "21"
-			verseEnd = "21"
-		elif bookId == "LUK":
-			chapterStart = "24"
-			verseStart = "54"
-			verseEnd = "54"
-		elif bookId == "JHN":
-			chapterStart = "21"
-			verseStart = "26"
-			verseEnd = "26"
-		else:
-			chapterStart = "0"
-	chapterEnd = None
-	verseStart = fileset[4]
-	if verseStart != None:
-		if "b" in verseStart:
-			verseStart = verseStart.replace("b", "")
-	verseEnd = fileset[5]
-	if verseEnd != None:
-		if "r" in verseEnd:
-			verseEnd = verseEnd.replace("r", "")
-		if "a" in verseEnd:
-			verseEnd = verseEnd.replace("a", "")
-	fileSize = None
-	duration = None
-
-	key = "%s-%s-%s-%s" % (hashId, bookId, chapterStart, verseStart)
-	if key in uniqueSet:
-		print("WARNING: dropping duplicate key ", fileset)
-	elif hashId not in files.hashIdSet:
-		print("WARNING: dropping hash_id mismatch ", fileset)
-	elif chapterStart == "0" and fileName[-5:] == ".html":
-		# This drop is probably not correct for production, correct feedback from App devs
-		print("WARNING: dropping zero chapter html files")
-	else:
-		uniqueSet.add(key)
-		results.append((hashId, bookId, chapterStart, chapterEnd, verseStart, verseEnd, fileName, fileSize, duration))
-
-print("num records to insert %d" % (len(results)))
-outputDB = SQLUtility(config.database_host, config.database_port,
-			config.database_user, config.database_output_db_name)
-outputDB.executeBatch("INSERT INTO bible_files (hash_id, book_id, chapter_start, chapter_end, verse_start, verse_end, file_name, file_size, duration) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)", results)
-outputDB.close()
 
 """
 app
