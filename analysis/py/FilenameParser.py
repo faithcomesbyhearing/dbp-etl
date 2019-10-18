fec418c6d9d9011138d3206b44853baa99a4add0
Gary N Griswold <gary@shortsands.com>
2019-10-18 11:23:12 -0400

    Rearrange Filename parser to have parse method in parser class.

--- analysis/py/FilenameParser.py
+++ analysis/py/FilenameParser.py
@@ -65,46 +65,56 @@ class Filename:
 		print(self.seq, self.book, self.chap, self.name, self.damid, self.type, self.file, self.error)
 
 
-	## {book_seq}___{chap}_{bookname}____{damid}.mp3 e.g. B01___01_Matthew_____ENGGIDN2DA.mp3
+class FilenameParser:
+
+	def __init__(self):
+		self.parsedList = []
+		self.unparsedList = []
+
+		## {book_seq}___{chap}_{bookname}____{damid}.mp3 e.g. B01___01_Matthew_____ENGGIDN2DA.mp3
 	def audioParser1(self, filename):
+		file = Filename(self.chapterMap)
 		parts = re.split("[_.]+", filename)
-		self.setType(parts[-1])
+		file.setType(parts[-1])
 		# split name from damid if there is no _ between
 		if any(c.islower() for c in parts[-2]):
 			newParts = self.splitDamId(parts[-2])
 			parts[-2] = newParts[1]
 			parts.insert(-2, newParts[0])
-		self.setDamid(parts[-2])
+		file.setDamid(parts[-2])
 		# combine name parts
 		if len(parts) > 4:
 			while (len(parts) > 5):
 				parts[2] = parts[2] + "_" + parts[3]
 				parts.pop(3)
 
-			self.setSeq(parts[0])
-			self.setBookName(parts[2])
-			self.setChap(parts[1])
+			file.setSeq(parts[0])
+			file.setBookName(parts[2])
+			file.setChap(parts[1])
 		else:
-			self.error.append("less than 5 parts")
-		filenameOut = self.seq + self.chap + self.name + self.damid + "." + self.type
+			file.error.append("less than 5 parts")
+		filenameOut = file.seq + file.chap + file.name + file.damid + "." + file.type
 		if filenameOut.replace("_", "") != filename.replace("_",""):
-			self.error.append("Mismatch %s" % (filenameOut))
+			file.error.append("Mismatch %s" % (filenameOut))
+		return file
 
 
 	## {book_seq}_{bookname}_{chap}_{damid}.mp3 e.g. B01_Genesis_01_S1COXWBT.mp3
 	def audioParser2(self, filename):
+		file = Filename(self.chapterMap)
 		parts = re.split("[_.]+", filename)
 		if len(parts) == 5:
-			self.setSeq(parts[0])
-			self.setBookName(parts[1])
-			self.setChap(parts[2])
-			self.setDamid(parts[3])
-			self.setType(parts[4])
+			file.setSeq(parts[0])
+			file.setBookName(parts[1])
+			file.setChap(parts[2])
+			file.setDamid(parts[3])
+			file.setType(parts[4])
 		else:
-			self.error.append("not 5 parts")
-		filenameOut = self.seq + self.name + self.chap + self.damid + "." + self.type
+			file.error.append("not 5 parts")
+		filenameOut = file.seq + file.name + file.chap + file.damid + "." + file.type
 		if filenameOut.replace("_", "") != filename.replace("_",""):
-			self.error.append("Mismatch %s" % (filenameOut))
+			file.error.append("Mismatch %s" % (filenameOut))
+		return file
 
 
 	def splitDamId(self, string):
@@ -113,17 +123,11 @@ class Filename:
 				return (string[:index + 1], string[index + 1:])
 
 
-class FilenameParser:
-
-	def __init__(self):
-		self.parsedList = []
-		self.unparsedList = []
-
 	def process(self):
 		db = SQLUtility("localhost", 3306, "root", "valid_dbp")
 		self.chapterMap = db.selectMap("SELECT id, chapters FROM books", None)
 		typeCode = 'audio'
-		filenamesMap = db.selectMapList("SELECT concat(type_code, '/', bible_id, '/', fileset_id), file_name FROM bucket_listing where type_code=%s", (typeCode))
+		filenamesMap = db.selectMapList("SELECT concat(type_code, '/', bible_id, '/', fileset_id), file_name FROM bucket_listing where type_code=%s limit 100000000000", (typeCode))
 		db.close()
 		for prefix in filenamesMap.keys():
 			#print(prefix)
@@ -131,11 +135,11 @@ class FilenameParser:
 
 			numErrors = 0
 			for filename in filenames:
-				parser = Filename(self.chapterMap)
-				parser.audioParser1(filename)
-				if len(parser.error) > 0:
+				#parser = Filename(self.chapterMap)
+				file = self.audioParser1(filename)
+				if len(file.error) > 0:
 					numErrors += 1
-					print("audio1", prefix, filename, ", ".join(parser.error))
+					print("audio1", prefix, filename, ", ".join(file.error))
 			if numErrors == 0:
 				self.parsedList.append(("audio1", prefix))
 			else:
@@ -163,7 +167,11 @@ class FilenameParser:
 		## if any method returns no errors, stop and save that one
 		## if any method returns similar errors, i.e. within 10 or within 10% report both.
 
-## we need a better index on bucket
+		## we need a better index on bucket
+
+	#def parsePrefix(self, prefix, parser, parserName):
+		### In order to get this to work, I think the parsers need to factory filename class instance
+		## that way the parse function is a method on this class.
 
 
 	def summary(self):
