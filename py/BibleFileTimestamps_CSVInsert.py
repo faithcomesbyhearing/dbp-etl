b4a3cd9514437bdfdbf597e05b92b230bbbd1169
Gary N Griswold <gary@shortsands.com>
2019-12-07 15:50:51 -0500

    BibleFileTimestamps_CSVInsert runs with good looking results.

---
+++ py/BibleFileTimestamps_CSVInsert.py
@@ -0,0 +1,138 @@
+# BibleFileTimestamps_CSVInsert.py
+#
+# This program loads timings data into the bible_file_timestamps table.
+# It reads one CSV file that contains the timings, and inserts the contents as one batch.
+#
+# Usage: python3 py/BibleFileTimestamps_CSVInsert.py  /top/dir/filename.csv quality_code
+# Sample Quality codes:
+# 0 = verified by known native speaker
+# 1 = verified by unknown native speaker
+# 2 = verified by non-native speaker
+# 3 = verified by non-speaker (eg members of the text team at FCBH who do this type of thing for a living)
+# 5 = automated spot-checked by non-native speaker (this is the case of most of the SAB appDef timings)
+# 9 = automated unverified (probably would never put these into the DB, but if I did at least I’d have this indicator of non-confidence)
+#
+
+import sys
+#import os
+import io
+import csv
+from SQLUtility import *
+
+
+TIM_HOST = "localhost"
+TIM_USER = "root"
+TIM_PORT = 3306
+TIM_DB_NAME = "dbp"
+
+
+class BibleFileTimestamps_CSVInsert:
+
+	def __init__(self):
+		self.db = SQLUtility(TIM_HOST, TIM_PORT, TIM_USER, TIM_DB_NAME)
+		self.bookCodeMap = self.db.selectMap("SELECT id_osis, id FROM books", ())
+		print("bookCodeMap", len(self.bookCodeMap.keys()))
+
+
+	def getCommandLine(self):
+		if len(sys.argv) < 3:
+			print("Usage: python3 py/BibleFileTimestamps_CSVInsert.py  /top/dir/filename.csv quality_code")
+			sys.exit()
+		path = sys.argv[1]
+		if not os.path.exists(path):
+			print("ERROR: File to load %s does not exist" % (path,))
+			sys.exit()
+		qualityCode = sys.argv[2]
+		if not qualityCode.isdigit():
+			print("ERROR: Quality code %s must be numeric" % (qualityCode))
+			sys.exit()
+		return (path, qualityCode)
+
+
+	def getFilesetIdList(self, path):
+		filesetIdSet = set()
+		with open(path, 'rt') as csvfile:
+			reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+			for row in reader:
+				if row[1] != "dam_id":
+					filesetIdSet.add(row[1])
+		return list(filesetIdSet)
+
+
+	def getFileIdMap(self, filesetIdList):
+		sql = ("SELECT concat(bs.id, ':', bf.book_id, ':', bf.chapter_start), bf.id FROM bible_files bf"
+			" INNER JOIN bible_filesets bs ON bf.hash_id = bs.hash_id"
+			" WHERE bs.set_type_code IN ('audio', 'audio_drama')"
+			" AND bs.id IN ") + "(" + filesetIdList + ")"
+		return self.db.selectMap(sql, ())
+
+
+	def getHashIdMap(self, filesetIdList):
+		sql = ("SELECT id, hash_id FROM bible_filesets WHERE asset_id = 'dbp-prod'"
+			" AND set_type_code IN ('audio', 'audio_drama')"
+			" AND id IN ") + "(" + filesetIdList + ")"
+		return self.db.selectMap(sql, ())
+
+
+	def process(self):
+		(path, qualityCode) = self.getCommandLine()
+		print(path, qualityCode)
+		filesetIdList = self.getFilesetIdList(path)
+		print("filesetId List", filesetIdList)
+		filesetIdStr = "'" + "','".join(filesetIdList) + "'"
+		print("filesetId List", filesetIdStr)
+		fileIdMap = self.getFileIdMap(filesetIdStr)
+		print("fileIdMap", len(fileIdMap.keys()))
+		hashIdMap = self.getHashIdMap(filesetIdStr)
+		print("hashIdMap", len(hashIdMap.items()))
+		values = []
+		with open(path, 'rt') as csvfile:
+			reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+			for row in reader:
+				if row[1] != "dam_id":
+					filesetId = row[1]
+					osisCode = row[2]
+					bookId = self.bookCodeMap.get(osisCode)
+					if bookId == None:
+						print("ERROR: Book code %s in line %s is not known" % (osisCode, row[0]))
+						sys.exit()
+					chapter = row[3]
+					verse = row[4]
+					timing = round(float(row[5]), 2)
+					key = "%s:%s:%s" % (filesetId, bookId, chapter)
+					fileId = fileIdMap.get(key)
+					if fileId == None:
+						print("ERROR: fileId is not found for %s" % (key))
+						sys.exit()
+					values.append((fileId, verse, None, timing))#, row))
+
+		cursor = self.db.conn.cursor()
+		print("Delete timestamps for filesets", filesetIdList)
+		print("Row %d to be inserted" % (len(values),))
+		deleteStmt = ("DELETE FROM bible_file_timestamps WHERE bible_file_id IN"
+			" (SELECT bf.id FROM bible_files bf, bible_filesets bs"
+			" WHERE bf.hash_id = bs.hash_id AND bs.id = %s)")
+		errorStmt = ("INSERT INTO bible_fileset_tags (hash_id, name, description, admin_only, iso, language_id)"
+			" VALUES (%s, 'timing_est_err', %s, 0, 'eng', 6414)")
+		insertStmt = "INSERT INTO bible_file_timestamps (bible_file_id, verse_start, verse_end, `timestamp`) VALUES (%s, %s, %s, %s)"
+		#for value in sorted(values):
+		#	print(value)
+		#print(len(values))
+		try:
+			for filesetId in filesetIdList:
+				cursor.execute(deleteStmt, (filesetId,))
+				hashId = hashIdMap[filesetId] # intend to crash if not found
+				cursor.execute(errorStmt, (hashId, qualityCode))
+			cursor.executemany(insertStmt, values)
+			self.db.conn.commit()
+			cursor.close()
+		except Exception as err:
+			self.db.error(cursor, insertStmt, err)
+
+		self.db.close()
+
+
+ins = BibleFileTimestamps_CSVInsert()
+ins.process()
+
+
--- py/BibleFileTimings_Copy.py
+++ py/BibleFileTimings_Copy.py
@@ -16,6 +16,13 @@
 # 8. During copy store the duration difference in bible_files.timing_est_err
 # 9. During copy store the source file_id in bible_files.timing_src of the file whose timing is copied
 
+# Note: there is also a need to recopy a fileset that has already been copied, because the source
+# timestamps have improved.
+
+# Note: if the BibleFileTimings_Insert program is given a reliability parameter,
+# how does this affect the ability to process over multiple Bibles.
+# Would it be better if the reliability were stored somewhere?
+
 # Consider storing timing_est_err in bible_file_tags for duration errors
 # Consider storing timing_est_err in bible_fileset_tags for raw copies error estimate
 # Consider storing timing_src in bible_file_tags which then does not have foreign key.
@@ -24,6 +31,7 @@
 import sys
 import os
 import io
+import statistics
 from SQLUtility import *
 
 
@@ -39,45 +47,109 @@ class BibleFileTimings_Copy:
 		self.db = SQLUtility(DUR_HOST, DUR_PORT, DUR_USER, DUR_DB_NAME)
 
 
+	# One option does all that can be done, skipping any already copied
+	# Another option does a specific bible_id, and does copy even when data exists
 	def getCommandLine(self):
 		if len(sys.argv) < 3:
-			print("Usage: BibleFileTimings_Copy.py starting_bible_id ending_bible_id")
+			print("Usage: BibleFileTimings_Copy.py source_err_limit_code duration_err_limit_in_sec [bible_id]")
 			sys.exit()
-		return(sys.argv[1], sys.argv[2])
+		bibleId = sys.argv[3] if len(sys.argv) == 4 else None
+		return(sys.argv[1], sys.argv[2], bibleId)
+
+
+	## return a source if it qualifies with quality limit, return all targets that have no source
+	def getFilesetSourceAndTargets(self, bibleId, qualityLimit):
+		sql = ("SELECT bf.id, bft.description AS timing_est_err"
+			" FROM bible_filesets bf" 
+			" INNER JOIN bible_fileset_connections bfc ON bf.hash_id = bfc.hash_id"
+			" LEFT OUTER JOIN bible_fileset_tags bft ON bf.hash_id = bft.hash_id"
+			" AND bft.name = 'timing_est_err'"
+			" WHERE bfc.bible_id = %s"
+			" AND bf.set_type_code IN ('audio', 'audio_drama')"
+			" ORDER BY bft.description")
+		resultSet = self.db.select(sql, (bibleId,))
+		targets = []
+		for row in resultSet:
+			if row[1] == None:
+				targets.append(row[0])
+			else:
+				if row[1] <= qualityLimit and len(targets) > 0:
+					return (row[0], targets)
+		return (None, None)
 
 
 	def selectDurations(self, filesetId):
 		sql = ("SELECT concat(book_id, ':', chapter_start), duration FROM bible_files WHERE hash_id IN"
 				" (SELECT hash_id FROM bible_filesets WHERE id = %s)")
 		return self.db.selectMap(sql, (filesetId,))
+
+
+	def copyTimings(self, srcFilesetId, destFilesetId):
+		sql = ("INSERT INTO bible_file_timestamps (bible_file_id, verse_start, verse_end, `timestamp`"
+			" SELECT ")
+		# insert select to copy the timings
 		
 
 	def process(self):
-		(startingId, endingId) = self.getCommandLine()
-		bibleIdList = self.db.selectList("SELECT id FROM bibles WHERE id BETWEEN %s AND %s", (startingId, endingId))
-		print(bibleIdList)
-		for bibleId in bibleIdList:
-			sql = ("SELECT id FROM bible_filesets bf, bible_fileset_connections bfc"
-					" WHERE bf.hash_id = bfc.hash_id AND bfc.bible_id = %s"
-					" AND bf.set_type_code IN ('audio', 'audio_drama')")
-			filesetIdList = self.db.selectList(sql, (bibleId,))	
-			print(filesetIdList)		
-			#startingFilesetId = filesetIdList[0]
-			startingFilesetId = "ENGESVC2DA16"
-			startingDurationMap = self.selectDurations(startingFilesetId) # book:chapter -> duration
-			for filesetId in filesetIdList:
-				if filesetId != startingFilesetId:
-					durationMap = self.selectDurations(filesetId)
-					for bookChapter, duration in startingDurationMap.items():
-						compareDuration = durationMap.get(bookChapter)
-						if compareDuration != None:# and abs(duration - compareDuration) > 1:
-							print("%s,%s,%s,%s,%d" % (bibleId, startingFilesetId, filesetId, bookChapter, (duration - compareDuration)))
-							#print(bibleId, startingFilesetId, filesetId, bookChapter, (duration - compareDuration))
+		(qualityLimit, durationErrLimit, bibleId) = self.getCommandLine()
+		if bibleId != None:
+			bibleIdList = [bibleId]
+		else:
+			bibleIdList = self.db.selectList("SELECT id FROM bibles")
+			print(bibleIdList)
+			for bibleId in bibleIdList:
+				(srcFilesetId, targetList = self.getFilesetSourceAndTargets(bibleId, qualityLimit)
+				if srcFilesetId != None:
+					print(srcFilesetId)
+					srcDurationMap = self.selectDurations(srcFilesetId) # book:chapter -> duration
+
+					for destFilesetId in targetList:
+						durationMap = self.selectDurations(destFilesetId)
+						durationDeltas = []
+						for bookChapter, duration in srcDurationMap.items():
+							compareDuration = durationMap.get(bookChapter)
+							if compareDuration != None:
+								durationDeltas.append((duration - compareDuration))
+								print("%s,%s,%s,%s,%d" % (bibleId, srcFilesetId, destFilesetId, bookChapter, (duration - compareDuration)))
+						avgDuration = statistics.mean(durationDeltas)
+						if avgDuration < durationErrLimit:
+							self.copyTimings(srcFilesetId, destFilesetId)
+
 		self.db.close()
 
 
 duration = BibleFileTimings_Copy()
 duration.process()
+
+## getFilesetSourceAndTarget
+"""
+SELECT bf.id, bft.description AS timing_est_err
+FROM bible_filesets bf 
+INNER JOIN bible_fileset_connections bfc ON bf.hash_id = bfc.hash_id
+LEFT OUTER JOIN bible_fileset_tags bft ON bf.hash_id = bft.hash_id AND bft.name = 'timing_est_err'
+WHERE bfc.bible_id = 'ENGESV'
+AND bf.set_type_code IN ('audio', 'audio_drama')
+ORDER BY bft.description;
+"""
+
+## Query to get all timings for a fileset
+## Do I need to do this one file at a time
+"""
+SELECT bft.bible_file_id, bft.verse_start, bft.verse_end, bft.`timestamp` 
+FROM bible_file_timestamps bft
+INNER JOIN bible_files bf ON bft.bible_file_id = bf.id
+INNER JOIN bible_filesets bs ON bf.hash_id = bs.hash_id
+AND bs.id = 'ENGKJVN2DA16'
+
+
+"""
+
+"""
+SELECT bf.id FROM bible_files bf
+JOIN bible_filesets bs ON bf.hash_id = bs.hash_id
+AND bs.id = 'ENGKJVN2DA';
+
+"""
 
 
 
