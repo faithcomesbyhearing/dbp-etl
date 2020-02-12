30904ca8d5d8eb42d04656c6454354aa4e0e5aaf
Gary N Griswold <gary@shortsands.com>
2020-02-11 20:35:52 -0500

    Started AccessGroupFilesetsTable

--- analysis/py/AccessGroupFilesetsTable.py
+++ analysis/py/AccessGroupFilesetsTable.py
@@ -39,15 +39,15 @@ class AccessGroupFilesetsTable:
 	def __init__(self, config, db):
 		self.config = config
 		self.db = db
-		self.PERMISSION_PUBLIC_DOMAIN = config.permission_public_domain # 2
-		self.PERMISSION_FCBH_GENERAL = config.permission_fcbh_general # 3  Don't know what is for
-		self.PERMISSION_FCBH_WEB = config.permission_fcbh_web # 4
-		self.PERMISSION_FCBH_GENERAL_EXCLUDE = config.permission_fcbh_general_exclude # 5 NOT USED
-		self.PERMISSION_DBS_GENERAL = config.permission_dbs_general # 6
-		self.PERMISSION_RESTRICTED = config.permission_restricted # 7
-		self.PERMISSION_GIDEONS_HIDE = config.permission_gideons_hide # 8
-		self.PERMISSION_FCBH_HUB = config.permission_fcbh_hub # 9
-		self.PERMISSION_BIBLEIS_HIDE = config.permission_bibleis_hide # 10
+		#self.PERMISSION_PUBLIC_DOMAIN = config.permission_public_domain # 2
+		#self.PERMISSION_FCBH_GENERAL = config.permission_fcbh_general # 3  Don't know what is for
+		#self.PERMISSION_FCBH_WEB = config.permission_fcbh_web # 4
+		#self.PERMISSION_FCBH_GENERAL_EXCLUDE = config.permission_fcbh_general_exclude # 5 NOT USED
+		#self.PERMISSION_DBS_GENERAL = config.permission_dbs_general # 6
+		#self.PERMISSION_RESTRICTED = config.permission_restricted # 7
+		#self.PERMISSION_GIDEONS_HIDE = config.permission_gideons_hide # 8
+		#self.PERMISSION_FCBH_HUB = config.permission_fcbh_hub # 9
+		#self.PERMISSION_BIBLEIS_HIDE = config.permission_bibleis_hide # 10
 
 
 	def process(self):
@@ -123,28 +123,49 @@ class AccessGroupFilesetsTable:
 		return results
 
 
+	def populateAccessGroups(self):
+		sql = "INSERT INTO access_groups (id, name, description) VALUES (%s, %s, %s)"
+		values = []
+		values.append((101, "allow_text_NT_DBP", "Allow NT Text on DBP (lpts: DBPText)"))
+		values.append((102, "allow_text_OT_DBP", "Allow OT Text on DBP (lpts: DBPTextOT)"))
+		values.append((103, "allow_audio_DBP", "Allow Audio on DBP (lpts: DBPAudio)"))
+		values.append((105, "allow_video_DBP", "Allow Video on DBP (lpts: - none -)"))
+
+		values.append((111, "allow_text_WEB", "Allow Text on Bible.is Website (lpts: HubText)"))
+		values.append((113, "allow_audio_WEB", "Allow Audio on Bible.is Website (lpts: DBPWebHub)"))
+		values.append((115, "allow_video_WEB", "Allow Video on Bible.is Website (lpts: WebHubVideo)"))
+
+		values.append((121, "allow_text_API", "Allow Text use through API (lpts: APIDevText)"))
+		values.append((123, "allow_audio_API", "Allow Audio use through API (lpts: APIDevAudio)"))
+		values.append((125, "allow_video_API", "Allow for Video use through API (lpts: APIDevVideo)"))
+
+		values.append((131, "allow_text_APP", "Allow Text on Bible.is App (lpts: MobileText)"))
+		values.append((133, "allow_audio_APP", "Allow Audio on Bible.is App (lpts: DBPMobile)"))
+		values.append((135, "allow_video_APP", "Allow Video on Bible.is App (lpts: MobileVideo)"))
+
+		values.append((141, "allow_text_GBA", "Allow Text in a Global Bible App (lpts: GBN_Text)"))
+		values.append((143, "allow_audio_GBA", "Allow Audio in a Global Bible App (lpts: GBN_Audio)"))
+		values.append((145, "allow_video_GBA", "Allow for Video in a Global Bible App (lpts: GBN_Video)"))
+
+		values.append((153, "allow_audio_RADIO", "Allow Audio Radio Streaming (lpts: Streaming)"))
+		values.append((155, "allow_video_RADIO", "Allow Video Radio Streaming (lpts: StreamingVideo)"))
+
+		values.append((163, "allow_audio_ITUNES", "Allow Audio on iTunes Podcast (lpts: ItunesPodcast)"))
+		values.append((165, "allow_video_ITUNES", "Allow Video on iTunes Podcast (lpts: ItunesPodcastVideo)"))
+
+		values.append((173, "allow_audio_SALES", "Allow Audio Sale (lpts: FCBHStore)"))
+		values.append((175, "allow_video_SALES", "Allow Video Sales (lpts: FCBHStoreVideo)"))
+
+		values.append((183, "allow_audio_DOWNLOAD", "Allow Audio Download (lpts: Download)"))
+		values.append((185, "allow_video_DOWNLOAD", "Allow Video Download (lpts: DownloadVideo)"))
+		self.db.executeBatch(sql, values)
+
+
 config = Config()
-db = SQLUtility(config.database_host, config.database_port,
-				config.database_user, config.database_output_db_name)
+db = SQLUtility(config)
 filesets = AccessGroupFilesetsTable(config, db)
-filesets.process()
+filesets.populateAccessGroups()
+#filesets.process()
 db.close()
-
-
-
-"""
-		Additional Names in Access, not extracted
-						"GBN_Text": "",
-						"GBN_Audio": "",
-						"FCBHStore": "",
-						"MobileVideo": "",
-						"GBN_Video": "",
-						"DownloadVideo": "",
-						"StreamingVideo": "",
-						"FCBHStoreVideo": "",
-						"ItunesPodcastVideo": "” }
-
-"""
-
 
 
