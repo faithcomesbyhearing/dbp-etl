e75b5e6576c055fb32e6a707872eb60a4d8ec0a0
Gary N Griswold <gary@shortsands.com>
2020-03-16 20:38:00 -0400

    Work in progress on UpdateDBPBiblesTable

--- load/UpdateDBPBiblesTable.py
+++ load/UpdateDBPBiblesTable.py
@@ -18,77 +18,122 @@ class UpdateDBPBiblesTable:
 		self.config = config
 		self.db = SQLUtility(config)
 		self.numeralIdMap = self.db.selectMap("SELECT script_id, numeral_system_id FROM alphabet_numeral_systems", ())
+		## These scripts have multiple numeral systems.  So, this hack chooses the dominant one
+		self.numeralIdMap["Arab"] = "eastern-arabic"
+		self.numeralIdMap["Deva"] = "devanagari"
 		## This should be added to DBP
 		self.numeralIdMap["Latn"] = "western-arabic"
+		self.yearPattern = re.compile("([0-9]+)")
 
 
-	def getBibleRecord(self, bibleId, lptsRecord, lptsIndex, setSizeCode):
-		languageId = self.biblesLanguageId(bibleId, lptsRecord)
-		versification = self.biblesVersification(bibleId, lptsRecord, lptsIndex)
-		script = self.biblesScript(bibleId, lptsRecord, lptsIndex)
+	def getBibleRecord(self, bibleId, lptsRecords, setSizeCode):
+		languageId = self.biblesLanguageId(bibleId, lptsRecords)
+		versification = None
+		script = self.biblesScript(bibleId, lptsRecords)
 		numeralSystemId = self.biblesNumeralId(script)
-		date = self.biblesDate(lptsRecord)
+		date = self.biblesDate(bibleId, lptsRecords)
 		scope = setSizeCode
 		derived = None
-		copyright = self.biblesCopyright(lptsRecord)
+		copyright = self.biblesCopyright(bibleId, lptsRecords)
 		priority = self.biblesPriority(bibleId)
 		reviewed = 0
 		notes = None
-		values = [(languageId, versification, numeralSystemId, date,
-			scope, script, derived, copyright, priority, reviewed, notes, bibleId)]
+		values = (languageId, versification, numeralSystemId, date,
+			scope, script, derived, copyright, priority, reviewed, notes, bibleId)
 		return values
 
 
-	def biblesLanguageId(self, bibleId, lptsRecord):
-		result = None
-		if lptsRecord != None:
+	def biblesLanguageId(self, bibleId, lptsRecords):
+		final = set()
+		for (lptsIndex, lptsRecord) in lptsRecords:
 			iso = lptsRecord.ISO()
 			langName = lptsRecord.LangName()
 			result = self.db.selectScalar("SELECT l.id FROM languages l,language_translations t WHERE l.iso=%s AND t.name=%s AND l.id=t.language_source_id", (iso, langName))
 			if result != None:
-				return result
-			result = self.db.selectScalar("SELECT id FROM languages WHERE iso=%s AND name=%s", (iso, langName))
-			if result != None:
-				return result
-		else:
+				final.add(result)
+			else:
+				result = self.db.selectScalar("SELECT id FROM languages WHERE iso=%s AND name=%s", (iso, langName))
+				if result != None:
+					final.add(result)
+		if len(final) == 0:
 			iso = bibleId[:3].lower()
-		result = self.db.selectScalar("SELECT id FROM languages WHERE iso=%s", (iso))
-		return result
+			result = self.db.selectScalar("SELECT id FROM languages WHERE iso=%s", (iso))
+			if result != None:
+				final.add(result)
+		return self.findResult(final, "LangName & ISO")
+		#if len(final) == 0:
+		#	return None
+		#elif len(final) == 1:
+		#	return list(final)[0]
+		#else:
+		#	print("ERROR: bible_id %s has multiple language_ids %s" % (bibleId, ",".join(final)))
+		#	return list(final)[0]
 
 
-	def biblesVersification(self, bibleId, lptsRecord, lptsIndex):
-		return None
-
-
-	def biblesScript(self, bibleId, lptsRecord, lptsIndex):
-		result = None
-		script = lptsRecord.Orthography(lptsIndex)
-		if script != None:
-			result = LookupTables.scriptCode(script)
-		return result
+	def biblesScript(self, bibleId, lptsRecords):
+		final = set()
+		for (lptsIndex, lptsRecord) in lptsRecords:
+			script = lptsRecord.Orthography(lptsIndex)
+			if script != None:
+				result = LookupTables.scriptCode(script)
+				final.add(result)
+		if len(final) == 0:
+			return None
+		elif len(final) == 1:
+			return list(final)[0]
+		else:
+			if "Latn" in final:
+				final.remove("Latn")
+			if len(final) == 1:
+				return list(final)[0]
+			else:
+				print("WARN: bible_id %s has multiple scripts |%s|" % (bibleId, "|".join(final)))
+				return list(final)[0]
 
 
 	def biblesNumeralId(self, script):
-		print(script)
-		## Bug: Arab and Deva have 2 numeral systems and I have no way to choose
 		numeralSystemId = self.numeralIdMap.get(script) 
-		print(self.numeralIdMap)
-		print(numeralSystemId)
 		return numeralSystemId
 
 
-	def biblesDate(self, lptsRecord):
-		volume = lptsRecord.Volumne_Name()
-		if volume != None:
-			yearPattern = re.compile("([0-9]+)")
-			match = yearPattern.search(volume)
+	def biblesDate(self, bibleId, lptsRecords):
+		final = set()
+		for (lptsIndex, lptsRecord) in lptsRecords:
+			self.extractYear(final, lptsRecord.Copyrightc())
+		if len(final) == 0:
+			for (lptsIndex, lptsRecord) in lptsRecords:
+				self.extractYear(final, lptsRecord.Volumne_Name())
+		return self.findResult(final, "Copyrightc Date")
+		#if len(final) == 0:
+		#	return None
+		#elif len(final) == 1:
+		#	return list(final)[0]
+		#else:
+		#	print("ERROR: bible_id %s has multiple Copyrightc dates %s" % (bibleId, ",".join(final)))
+		#	return list(final)[0]
+
+
+	def extractYear(self, finalSet, value):
+		if value != None:
+			match = self.yearPattern.search(value)
 			if match != None:
-				return match.group(1)
-		return None
+				finalSet.add(match.group(1))
 
 
-	def biblesCopyright(self, lptsRecord):
-		return lptsRecord.Copyrightc()
+	def biblesCopyright(self, bibleId, lptsRecords):
+		final = set()
+		for (lptsIndex, lptsRecord) in lptsRecords:
+			result = lptsRecord.Copyrightc()
+			if result != None:
+				final.add(result)
+		return self.findResult(final, "Copyrightc")
+		#if len(final) == 0:
+		#	return None
+		#elif len(final) == 1:
+		#	return list(final)[0]
+		#else:
+		#	print("ERROR: bible_id %s has multiple Copyrightc values |%s|" % (bibleId, "|".join(final)))
+		#	return list(final)[0]
 
 
 	def biblesPriority(self, bibleId):
@@ -105,40 +150,40 @@ class UpdateDBPBiblesTable:
 		return priority.get(bibleId, 0)
 
 
-	def findTextBibleLPTSRecord(self, bibleId, lptsReader):
-		lptsRecords = lptsReader.bibleIdMap.get(bibleId)
-		if len(lptsRecords) == 0:
-			print("ERROR: %s has not LPTS records" % (bibleId))
-			sys.exit()
+	def findResult(self, final, fieldName):
+		if len(final) == 0:
+			return None
+		elif len(final) == 1:
+			return list(final)[0]
+		else:
+			print("WARN: bible_id %s has multiple %s values |%s|" % (bibleId, fieldName, "|".join(final)))
+			return list(final)[0]
+
+
+	def findTextBibleLPTSRecords(self, bibleId, lptsReader):
 		results = []
+		lptsRecords = lptsReader.bibleIdMap.get(bibleId, [])
 		for (lptsIndex, lptsRecord) in lptsRecords:
 			damids = lptsRecord.DamIds("text", lptsIndex)
 			if len(damids) > 0:
 				results.append((lptsIndex, lptsRecord))
-		if len(results) == 1:
-			return results[0]
-		elif len(results) > 1:
-			for (index, record) in results:
-				print(index, record.DBP_Equivalent(), record.Reg_StockNumber())
-			print("ERROR: %s has too many LPTS text records" % (bibleId))
-			sys.exit()
-		results = []
-		print("Starting loose select for %s" % (bibleId))
-		print("We need a loose procedure")
-		sys.exit()
+		return results
 
 
 ## Unit Test
 if (__name__ == '__main__'):
-	#bibleId = "ENGESV"
-	#bibleId = "ENGKJV"
-	#bibleId = "ENGASV"
-	bibleId = "ENGCEV"
-	setSizeCode = "NTOT"
+	setSizeCode = "NTOT" ## This hard-coding needs to be replaced with getting from fileset
 	config = Config()
+	db = SQLUtility(config)
+	bibleIds = db.selectList("SELECT distinct bible_id FROM bible_fileset_connections", ())
+	db.close()
 	lptsReader = LPTSExtractReader(config)
 	update = UpdateDBPBiblesTable(config)
-	(lptsIndex, lptsRecord) = update.findTextBibleLPTSRecord(bibleId, lptsReader)
-	values = update.getBibleRecord(bibleId, lptsRecord, lptsIndex, setSizeCode)
-	print(values)
+	update.counts = 0
+	for bibleId in bibleIds:
+		lptsRecords = update.findTextBibleLPTSRecords(bibleId, lptsReader)
+		values = update.getBibleRecord(bibleId, lptsRecords, setSizeCode)
+		#values = update.getBibleRecord(bibleId, lptsRecord, lptsIndex, setSizeCode)
+		print(values)
+	print("count", update.counts)
 
