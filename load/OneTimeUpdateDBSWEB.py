0a43b1b15c9250d9497ea42fe166727e546f3fae
Gary N Griswold <gary@shortsands.com>
2020-03-28 18:51:11 -0400

    OneTimeUpdateDBSWEB.py is doing correct analysis

--- load/OneTimeUpdateDBSWEB.py
+++ load/OneTimeUpdateDBSWEB.py
@@ -27,6 +27,8 @@ class BibleFileset:
 		self.dbsFormat = None
 		self.dbsPlain = None
 		self.prodFiles = 0
+		self.dbsFormat2fcbhFormat = False
+		self.dbsPlain2fcbhPlain = False
 		for row in resultSet:
 			record = BibleFilesetRecord(row[2], row[3])
 			if row[0] == "dbp-prod" and row[1] == "text_format":
@@ -37,13 +39,6 @@ class BibleFileset:
 				self.dbsFormat = record
 			elif row[0] == "dbs-web" and row[1] == "text_plain":
 				self.dbsPlain = record
-
-#	def hasContent(self):
-#		return (self.fcbhFormat != None or
-#				self.fcbhPlain != None or
-#				self.dbsFormat != None or
-#				self.dbsPlain != None or
-#				self.prodFiles > 0)
 
 	def hasDBSWEBContent(self):
 		return self.dbsFormat != None or self.dbsPlain != None
@@ -58,7 +53,6 @@ class BibleFileset:
 			result.append("%s: %s" % ("dbs-format", self.dbsFormat.toString()))
 		if self.dbsPlain != None:
 			result.append("%s: %s" % ("dbs-plain", self.dbsPlain.toString()))
-		#if self.prodFiles > 0:
 		result.append("dbp-prod: %d" % (self.prodFiles))
 		return "\n".join(result)
 
@@ -75,37 +69,33 @@ class OneTimeUpdateDBSWEB:
 
 
 	def process(self):
+		count = 0
 		dbpProdMap = self.getDBPProdFiles()
-		allFilesets = []
 		for (bibleId, filesetId) in self.getBibleFilesetList():
-			#print("1", bibleId, filesetId)
 			filesetList = self.getFileset(bibleId, filesetId)
 			filesetList.prodFiles = dbpProdMap.get(bibleId + "/" + filesetId, 0)
 			if filesetList.hasDBSWEBContent():
-				print("2", bibleId, filesetId)
+				print(bibleId, filesetId)
+				self.getNumVerses(filesetList)
 				print(filesetList.toString())
-				self.processTextFormat(filesetList)
-			#allFilesets.append(getFileset(bibleId, filesetId))
-			# populate with verse information
+				self.processTextFormat(bibleId, filesetId, filesetList)
+				self.processTextPlain(bibleId, filesetId, filesetList)
+				count += 1
+				#if count > 100:
+				#	break
 
 		
 	def getDBPProdFiles(self):
 		fileCountMap = {}
-		#print(self.config.directory_bucket_list + "dbp-prod.txt")
 		fp = open(self.config.directory_bucket_list + "dbp-prod.txt")
 		for line in fp:
 			if line.startswith("text/"):
-				#print(line)
 				parts = line.split("/")
 				if len(parts) > 3 and len(parts[1]) < 20 and len(parts[2]) < 20:
 					key = parts[1] + "/" + parts[2]
-					#print(key)
 					count = fileCountMap.get(key, 0)
 					count += 1
 					fileCountMap[key] = count
-		#for fileset in sorted(fileCountMap.keys()):
-		#	count = fileCountMap[fileset]
-		#	print(fileset, count)
 		fp.close()
 		return fileCountMap
 
@@ -130,25 +120,50 @@ class OneTimeUpdateDBSWEB:
 		#	print(row)
 		return BibleFileset(resultSet)
 
-
-	def getVerses(self, hashId):
-		sql = ("SELECT count(*) FROM bible_verses WHERE hash_id = %s")
-		count = self.db.selectScalar(sql, (hashId))
-		return count
-
-
-	def processTextFormat(self, fileset):
+	def getNumVerses(self, fileset):
+		sql = "SELECT count(*) FROM bible_verses WHERE hash_id = %s"
 		if fileset.dbsFormat != None:
+			fileset.dbsFormat.numVerses = self.db.selectScalar(sql, (fileset.dbsFormat.hashId))
+		if fileset.dbsPlain != None:
+			fileset.dbsPlain.numVerses = self.db.selectScalar(sql, (fileset.dbsPlain.hashId))
+		if fileset.fcbhFormat != None:
+			fileset.fcbhFormat.numVerses = self.db.selectScalar(sql, (fileset.fcbhFormat.hashId))
+		if fileset.fcbhPlain != None:
+			fileset.fcbhPlain.numVerses = self.db.selectScalar(sql, (fileset.fcbhPlain.hashId))
+
+	def processTextFormat(self, bibleId, filesetId, fileset):
+		if fileset.dbsFormat != None:
+			hashId = fileset.dbsFormat.hashId
 			if fileset.dbsFormat.numFiles > 10 and fileset.prodFiles > 10:
 				if fileset.fcbhFormat == None:
-					print("CHANGE %s dbs-web text_format to dbp-prod" % (fileset.dbsFormat.hashId))
-				elif fileset.fcbhFormat != None and fileset.fcbhFormat.numFiles == 0:
-					print("DELETE %s dbp-prod text_format" % (fileset.fcbhFormat.hashId))
-					print("CHANGE %s dbs-web text_format to dbp-prod" % (fileset.dbsFormat.hashId))
+					fileset.dbsFormat2fcbhFormat = True
+					print("CHANGE1 %s %s/%s dbs-web text_format to dbp-prod" % (hashId, bibleId, filesetId))
+				elif fileset.fcbhFormat != None and (fileset.fcbhFormat.numFiles == 0 or fileset.prodFiles == 0):
+					fileset.dbsFormat2fcbhFormat = True
+					print("DELETE2 %s %s/%s dbp-prod text_format" % (fileset.fcbhFormat.hashId, bibleId, filesetId))
+					print("CHANGE1 %s %s/%s dbs-web text_format to dbp-prod" % (hashId, bibleId, filesetId))
 			else:
-				print("DELETE %s dbs-web text_format has no files in dbp-prod." % (fileset.dbsFormat.hashId))
+				print("DELETE %s %s/%s dbs-web text_format has no files in dbp-prod." % (hashId, bibleId, filesetId))
 			if fileset.dbsFormat.numVerses > 100:
-				print("WARN: %s dbs-web text_format has %d verses." % (fileset.dbsFormat.hashId, fileset.dbsFormat.numFiles))
+				print("WARN: %s %s/%s dbs-web text_format has %d verses." % (hashId, fileset.dbsFormat.numFiles, bibleId, filesetId))
+
+
+	def processTextPlain(self, bibleId, filesetId, fileset):
+		if fileset.dbsPlain != None:
+			hashId = fileset.dbsPlain.hashId
+			if fileset.fcbhFormat != None or fileset.dbsFormat2fcbhFormat:
+				if fileset.dbsPlain.numVerses > 100:
+					if fileset.fcbhPlain == None:
+						fileset.dbsPlain2fcbhPlain = True
+						print("CHANGE3 %s %s/%s dbs-web text_plain to db" % (hashId, bibleId, filesetId))
+					elif fileset.fcbhPlain != None and fileset.fcbhPlain.numVerses == 0:
+						fileset.dbsPlain2fcbhPlain = True
+						print("DELETE4 %s %s/%s dbp-prod text_plain" % (fileset.fcbhFormat.hashId, bibleId, filesetId))
+						print("CHANGE3 %s %s/%s dbs-web text_plain to db" % (hashId, bibleId, filesetId))
+			if fileset.dbsPlain.numVerses == 0:
+				print("WARN: %s %s/%s dbs-web text_plain has no verses" % (hashId, bibleId, filesetId))
+			if fileset.dbsPlain.numFiles > 0:
+				print("WARN: %s %s/%s dbs-web text_plain has %d files" % (hashId, bibleId, filesetId, fileset.dbsPlain.numFiles))
 
 	"""
 	dbs-web 1647
@@ -169,26 +184,6 @@ class OneTimeUpdateDBSWEB:
 
 	There are no bible_verses that have a set_type_code of text_format
 
-	1) build a structure that is (hash_id, bible_id, fileset_id, asset_id, set_type_code, num_files, num_verses)
-	2) have as 4 groups dbp-prod, text_plain, dbp-prod, text_format, dbs-web, text_plain, dbs-web, text_format
-
-	Decision Box
-	conditions:
-	has files, per DBP
-	has verses, per DBP
-	has files, per dbp-prod
-
-	A. Iterate over each bible/fileset pair in DBP
-	B. Put data info an object:
-		dbs-web/text_format:
-		dbp-prod/text_format:
-		dbs-web/text_plain:
-		dbp-prod/text_plain:
-		num_dbp_prod_files:
-		each carries a record: (hash_id, num_verses, num_files)
-	C. Iterate over DBP-PROD to populate num_files field
-	D. This logic ignores things in bucket, but no DBP, which is OK. That will be solved later.
-
 	Process text_format
 	1) if dbs-web exists and has files and dbp-prod does not exist -> change to dbp-prod
 	2) if dbs-web exists and has files and dbp-prod exists, but no files -> change dbs-web to dbp-prod
@@ -205,6 +200,5 @@ class OneTimeUpdateDBSWEB:
 if (__name__ == '__main__'):
 	once = OneTimeUpdateDBSWEB()
 	fsMap = once.process()
-	#print(fsMap)
 
 
