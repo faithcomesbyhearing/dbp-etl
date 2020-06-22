261bc2c31862fe05a8587edc1425ab1aa2ce52c0
Gary N Griswold <gary@shortsands.com>
2020-06-22 17:36:22 -0400

    MatchOrganizationNames updated.

--- load/MatchOrganizationNames.py
+++ load/MatchOrganizationNames.py
@@ -7,6 +7,8 @@
 import io
 import os
 import sys
+import csv
+import operator
 from datetime import datetime
 from Config import *
 from LPTSExtractReader import *
@@ -22,14 +24,35 @@ class MatchOrganizationNames:
 		self.licensorsNoMatch = set()
 		self.copyHolderNoMatch = set()
 		self.organizationMap = {}
-		resultSet = self.db.select("SELECT id, slug FROM organizations", ())
-		for row in resultSet:
-			self.organizationMap[row[0]] = (row[0], row[1], set(), set())
-		self.langMap = self.db.selectMap("SELECT name, organization_id FROM organization_translations", ())
+		#resultSet = self.db.select("select id, slug from organizations where id in (select organization_id from bible_fileset_copyright_organizations) order by slug", ())
+		with open("/Volumes/FCBH/database_data/organizations.csv", newline="\n") as csvfile:
+			reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+			for row in reader:
+				self.organizationMap[int(row[0])] = (int(row[0]), row[1], set(), set())
+		self.langMap = {}
+		#self.langMap = self.db.selectMap("SELECT name, organization_id FROM organization_translations where organization_id in (select organization_id from bible_fileset_copyright_organizations) order by name", ())
+		with open("/Volumes/FCBH/database_data/organization_translations.csv", newline="\n") as csvfile:
+			reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+			for row in reader:
+				self.langMap[row[0]] = int(row[1])
+
+
+	def hasDamIds(self, lptsRecord, typeCode):
+		damIds = lptsRecord.DamIds(typeCode, 1)
+		if len(damIds) > 0:
+			return True
+		damIds = lptsRecord.DamIds(typeCode, 2)
+		if len(damIds) > 0:
+			return True
+		damIds = lptsRecord.DamIds(typeCode, 3)
+		if len(damIds) > 0:
+			return True
+		return False
 
 
 	def processLicensor(self):
 		for record in self.lptsReader.resultSet:
+			#if record.hasDamIds(record, "text"):
 			if record.Licensor() != None:
 				self.matchLicensor(record.Licensor())
 			if record.CoLicensor() != None:
@@ -65,7 +88,9 @@ class MatchOrganizationNames:
 			if record.Copyright_Video() != None:
 				self.matchCopyHolder(record.Copyright_Video())
 
-		print(len(self.copyHolderNoMatch), "Copyright Holdersthat do not match")
+		for item in self.copyHolderNoMatch:
+			print("|" + item + "|")
+		print(len(self.copyHolderNoMatch), "Copyright Holders that do not match")
 		count = 0
 		for (orgId, slug, licensors, copyHolders) in self.organizationMap.values():
 			if len(copyHolders) > 0:
@@ -75,36 +100,50 @@ class MatchOrganizationNames:
 		print(count, "Copyright Holders that do match")	
 
 
-
 	def matchCopyHolder(self, copyHolder):
 		copyright = self.reduceName(copyHolder)
-		orgId = self.langMap.get(copyHolder)
+		orgId = self.langMap.get(copyright)
 		if orgId != None:
 			organization = self.organizationMap[orgId]
 			(orgId, slug, licensors, copyHolders) = organization
-			copyHolders.add(copyHolder)
+			copyHolders.add(copyright)
 			self.organizationMap[orgId] = (orgId, slug, licensors, copyHolders)
 		else:
-			self.copyHolderNoMatch.add(copyHolder)
+			self.copyHolderNoMatch.add(copyright)
 
 
 	def reduceName(self, name):
-		return name
+		for char in ["©", "℗", "®","0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ",", "/", "-", ";", "_", ".","\n"]:
+			name = name.replace(char, "")
+		name = name.replace("     ", " ")
+		name = name.replace("    ", " ")
+		name = name.replace("   ", " ")
+		name = name.replace("  ", " ")
+		name = name[:64]
+		return name.strip()
 
 
+	def displayOutput(self):
+		orgTuples = self.organizationMap.values()
+		organizations = sorted(orgTuples, key=operator.itemgetter(1))
+		with open("organizations_with_match.csv", "w", newline="\n") as csvfile:
+			writer = csv.writer(csvfile, delimiter=",", quotechar='"')
+			for (orgId, slug, licensors, copyHolders) in organizations:
+				licensor = "\n".join(licensors)
+				copyHolder = "\n".join(copyHolders)
+				writer.writerow([orgId, slug, licensor, copyHolder])
 
+		sortedLicensors = sorted(self.licensorsNoMatch)
+		fp = open("licensors_no_match.csv", "w")
+		for item in sortedLicensors:
+			fp.write(item + "\n")
+		fp.close()
 
-
-
-
-
-
-
-
-
-
-
-
+		sortedCopyHolders = sorted(self.copyHolderNoMatch)
+		fp = open("copyright_holders_no_match.csv", "w")
+		for item in sortedCopyHolders:
+			fp.write(item + "\n")
+		fp.close()
 
 
 
@@ -115,4 +154,6 @@ if (__name__ == '__main__'):
 	test = MatchOrganizationNames(config, db, lptsReader)
 	test.processLicensor()
 	test.processCopyHolder()
+	test.displayOutput()
 	db.close()
+
