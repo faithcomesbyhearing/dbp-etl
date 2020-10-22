b6e9e8632e4b3fb637f0643feb62cb83d754f91f
Gary N Griswold <gary@shortsands.com>
2020-10-22 00:08:49 -0400

    S3Utility upgraded to handle video and audio

--- load/S3Utility.py
+++ load/S3Utility.py
@@ -61,67 +61,25 @@ class S3Utility:
 		except Exception as err:
 			print("ERROR: Upload %s failed  with error %s" % (s3Key, err))
 
-	## deprecated
-	def uploadAllFilesetsOld(self):
-		directory = self.config.directory_uploading
-		lenDirectory = len(directory)
-		for root, dirs, files in os.walk(directory):
-			relDirName = root[lenDirectory:].replace("\\", "/")
-			if relDirName[:5] == "audio" and len(dirs) == 0:
-				#print("***** inside audio", directory, relDirName, dirs)
-				self.uploadAudioFileset(self.config.s3_bucket, directory, relDirName, files)
-			#elif relDirName[:4] == "text" and relDirName.count("/") == 1:
-			#	print("***** inside text", directory, relDirName, dirs)
-			#else:
-			#	print("***** ELSEWHERE", directory, relDirName, dirs)
-
-	## deprecated
-	def uploadAudioFilesetOld(self, s3Bucket, sourceDir, filesetPrefix, files):
-		errorCount = 0
-		targetDir = self.config.directory_uploaded
-		os.makedirs(targetDir + filesetPrefix, exist_ok=True)
-		for file in sorted(files):
-			if not file.startswith("."):
-				s3Key = filesetPrefix + os.sep + file
-				filename = sourceDir + s3Key
-				try:
-					print("upload: %s" % (s3Key))
-					self.client.upload_file(filename, s3Bucket, s3Key,
-					ExtraArgs={'ContentType': "audio/mpeg"})
-					moveFilename = targetDir + s3Key
-					os.rename(filename, moveFilename)
-				except Exception as err:
-					print("ERROR: Upload of %s to %s failed: %s" % (s3Key, s3Bucket, err))
-					errorCount += 1
-
-		if errorCount == 0:
-			self._cleanupDirectory(sourceDir, filesetPrefix)
-			self.promoteFileset(targetDir, filesetPrefix)
-
 
 	def uploadAllFilesets(self):
-		uploadSucceeded = 0
-		uploadFailed = 0
 		directory = self.config.directory_upload
 		for typeCode in os.listdir(directory):
-			if typeCode in {"audio", "text", "video"}:
+			if typeCode in {"audio", "video"}: # "text"
+				s3Bucket = self.config.s3_vid_bucket if typeCode == "video" else self.config.s3_bucket
 				for bibleId in [f for f in os.listdir(directory + typeCode) if not f.startswith('.')]:
 					for filesetId in [f for f in os.listdir(directory + typeCode + os.sep + bibleId) if not f.startswith('.')]:
-						if typeCode == "audio":
-							filesetPrefix = typeCode + "/" + bibleId + "/" + filesetId + "/"
-							done = self.uploadAudioFileset(self.config.s3_bucket, directory, filesetPrefix)
-							if done:
-								uploadSucceeded += 1
-							else:
-								uploadFailed += 1
-		print("%d filesets uploaded" % (uploadSucceeded))
-		print("%d filesets failed to upload" % (uploadFailed))
+						filesetPrefix = typeCode + "/" + bibleId + "/" + filesetId + "/"
+						done = self.uploadFileset(s3Bucket, directory, filesetPrefix)
+						if done:
+							print("Upload %s succeeded." % (filesetPrefix,))
+						else:
+							print("Upload %s FAILED." % (filesetPrefix,))
 
 
-	def uploadAudioFileset(self, s3Bucket, sourceDir, filesetPrefix):
+	def uploadFileset(self, s3Bucket, sourceDir, filesetPrefix):
 		print("uploading: ", filesetPrefix)
-		cmd = "aws s3 sync %s%s s3://%s/%s" % (sourceDir, filesetPrefix, s3Bucket, filesetPrefix)
-		#cmd = "aws s3 cp %s%s s3://%s/%s --recursive" % (sourceDir, filesetPrefix, s3Bucket, filesetPrefix)
+		cmd = "aws --profile %s s3 sync %s%s s3://%s/%s" % (self.config.s3_aws_profile, sourceDir, filesetPrefix, s3Bucket, filesetPrefix)
 		response = subprocess.run(cmd, shell=True, stderr=subprocess.PIPE)
 		if response.returncode != 0:
 			print("ERROR: Upload of %s to %s failed. MESSAGE: %s" % (filesetPrefix, s3Bucket, response.stderr))
