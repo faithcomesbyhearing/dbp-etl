f5bdedb81ebde6d620c88aad1bb601c08cfb7ec1
Gary N Griswold <gary@shortsands.com>
2020-12-10 15:08:44 -0500

    Add exception handling to LPTS Extract

--- load/LPTSExtractReader.py
+++ load/LPTSExtractReader.py
@@ -14,7 +14,12 @@ class LPTSExtractReader:
 
 	def __init__(self, config):
 		self.resultSet = []
-		doc = minidom.parse(config.filename_lpts_xml)
+		dom = None
+		try:
+			doc = minidom.parse(config.filename_lpts_xml)
+		except Exception as err:
+			print("FATAL ERROR parsing LPTS extract", err)
+			sys.exit(1)
 		root = doc.childNodes
 		if len(root) != 1 or root[0].nodeName != "dataroot":
 			print ("ERROR: First node of LPTS Export must be <docroot>")
