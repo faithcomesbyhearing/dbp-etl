9269a796d2706423b59514a9fed2b9699f705917
Garrett Hopper <garrett@garretthopper.com>
2021-03-10 14:48:39 -0600

    make s3_aws_profile optional

--- load/Config.py
+++ load/Config.py
@@ -73,7 +73,7 @@ class Config:
 		self.s3_bucket = self._get("s3.bucket")
 		self.s3_vid_bucket = self._get("s3.vid_bucket")
 		self.s3_artifacts_bucket = self._get("s3.artifacts_bucket")
-		self.s3_aws_profile = self._get("s3.aws_profile")
+		self.s3_aws_profile = self._getOptional("s3.aws_profile")
 		session = boto3.Session(profile_name=self.s3_aws_profile)
 		self.s3_client = session.client('s3')
 
