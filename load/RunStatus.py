08486c527bd8c4da067496f2beb8c95b153c2343
Gary N Griswold <gary@shortsands.com>
2021-04-01 19:53:56 -0400

    Rewrote RunStatus to a new program.

--- load/RunStatus.py
+++ load/RunStatus.py
@@ -1,7 +1,6 @@
 # RunStatus.py
 
 
-
 import boto3
 import time
 from Config import *
@@ -9,21 +8,37 @@ from DBPRunFilesS3 import *
 
 
 class RunStatus:
-	UNKNOWN = "UNKNOWN"
-	PREPROCESS = "BEGIN PREPROCESS"
-	VALIDATE = "BEGIN VALIDATE"
-	UPLOAD = "BEGIN UPLOAD"
-	UPDATE = "BEGIN UPDATE"
-	SUCCESS = "SUCCESS"
-	FAILURE = "FAILURE"
+	BIBLE = "BIBLE"
+	LPTS = "LPTS"
 
-	current = UNKNOWN
 	start = time.time()
+	statusMap = {}
+	statusList = []
 
 
-	def setStatus(status):
-		RunStatus.current = status
-		RunStatus.printDuration(status)
+	def init(filesets):
+		results = []
+		results.append(RunStatus.BIBLE)
+		for inp in filesets:
+			results.append(inp.filesetId)
+		results.append(RunStatus.LPTS)
+		RunStatus.statusList = results
+		RunStatus.store()		
+
+
+	def set(name, status):
+		msg = "ok" if status else "failed"
+		RunStatus.statusMap[name] = msg
+		print("********** %s Tables Update %s **********" % (name, msg,))
+		RunStatus.store()
+
+
+	def store():
+		results = []
+		for name in RunStatus.statusList:
+			status = RunStatus.statusMap.get(name, "not_done")
+			results.append(name + ": " + status)
+		statusMsg = ", ".join(results)
 		client = Config.shared().s3_client
 		bucket = Config.shared().s3_artifacts_bucket
 		key = DBPRunFilesS3.s3KeyPrefix + "/metadata"
@@ -31,13 +46,13 @@ class RunStatus:
 		try:
 			response = client.get_object(Bucket=bucket, Key=key)
 			metadata = response.get('Metadata')
-			#print(metadata.get("x-run-status"))
 		except client.exceptions.NoSuchKey:
 			metadata = {}
 		except Exception as err:
 			print("RunStatus Metadata Download error", err)
 		try:
-			metadata["status"] = RunStatus.current
+			metadata["status"] = statusMsg
+			print("WRITE", metadata.get("status"))
 			client.put_object(Bucket=bucket, Key=key, Metadata=metadata, ACL='bucket-owner-full-control')
 		except Exception as err:
 			print("RunStatus Metadata Upload error", err)
@@ -50,16 +65,21 @@ class RunStatus:
 		RunStatus.start = now
 
 
-
 if __name__ == "__main__":
+	from LPTSExtractReader import *
+	from InputFileset import *
+
+	config = Config.shared()
+	lptsReader = LPTSExtractReader(config.filename_lpts_xml)
+	filesets = InputFileset.filesetCommandLineParser(config, lptsReader)
+	RunStatus.init(filesets)
 	time.sleep(1)
-	RunStatus.setStatus(RunStatus.VALIDATE)
+	RunStatus.set(RunStatus.BIBLE, True)
 	time.sleep(2)
-	RunStatus.setStatus(RunStatus.UPLOAD)
-	time.sleep(3)
-	RunStatus.printDuration("TRANSCODE SUBMIT")
-	time.sleep(2)
-	RunStatus.printDuration("TRANSCODE CHECK")
-	time.sleep(1)
-	RunStatus.printDuration("TRANSCODE DONE")
+	for fileset in filesets:
+		RunStatus.set(fileset.filesetId, False)
+		time.sleep(2)
+	RunStatus.set(RunStatus.LPTS, False)
+
+# python3 load/RunStatus.py test s3://test-dbp audio/ENGESV/ENGESVN2DA  audio/ENGESV/ENGESVN2DA16
 
