3152d8d4106a2355b1cf77195ea376fb4757984f
Gary N Griswold <gary@shortsands.com>
2021-05-13 10:43:48 -0400

    Addition of 181, 191, 193 access_groups

--- load/UpdateDBPAccessTable.py
+++ load/UpdateDBPAccessTable.py
@@ -68,6 +68,10 @@ class UpdateDBPAccessTable:
 						accessIdInLPTS = False
 				elif accessId == 145: # allow_video_GBA
 					accessIdInLPTS = lpts.get("APIDevVideo") == "-1"
+				elif accessId == 181: # allow_text_DOWNLOAD
+					accessIdInLPTS = self._isPublicDomain(lptsRecord)
+				elif accessId in {191, 193}: # allow_text_APP_OFFLINE and allow_audio_APP_OFFLINE
+					accessIdInLPTS = self._isSILOnly(lptsRecord)
 				else:
 					accessIdInLPTS = lpts.get(accessDesc) == "-1"
 
@@ -82,93 +86,29 @@ class UpdateDBPAccessTable:
 		self.dbOut.delete(tableName, pkeyNames, deleteRows)
 
 
-	## deprecated
-	def insertAccessGroups(self):
-		count = self.db.selectScalar("SELECT count(*) FROM access_groups WHERE id > 100", ())
-		if count > 0:
-			return
-		sql = "INSERT INTO access_groups (id, name, description) VALUES (%s, %s, %s)"
-		values = []
-		values.append((101, "allow_text_NT_DBP", "DBPText"))
-		values.append((102, "allow_text_OT_DBP", "DBPTextOT"))
-		values.append((103, "allow_audio_DBP", "DBPAudio"))
-		#values.append((105, "allow_video_DBP", NULL))
-
-		values.append((111, "allow_text_WEB", "HubText"))
-		values.append((113, "allow_audio_WEB", "DBPWebHub"))
-		values.append((115, "allow_video_WEB", "WebHubVideo"))
-
-		values.append((121, "allow_text_API", "APIDevText"))
-		values.append((123, "allow_audio_API", "APIDevAudio"))
-		values.append((125, "allow_video_API", "APIDevVideo"))
-
-		values.append((131, "allow_text_APP", "MobileText"))
-		values.append((133, "allow_audio_APP", "DBPMobile"))
-		values.append((135, "allow_video_APP", "MobileVideo"))
-
-		values.append((141, "allow_text_GBA", "GBN_Text"))
-		values.append((143, "allow_audio_GBA", "GBN_Audio"))
-		values.append((145, "allow_video_GBA", "GBN_Video"))
-
-		values.append((153, "allow_audio_RADIO", "Streaming"))
-		values.append((155, "allow_video_RADIO", "StreamingVideo"))
-
-		values.append((163, "allow_audio_ITUNES", "ItunesPodcast"))
-		values.append((165, "allow_video_ITUNES", "ItunesPodcastVideo"))
-
-		values.append((173, "allow_audio_SALES", "FCBHStore"))
-		values.append((175, "allow_video_SALES", "FCBHStoreVideo"))
-
-		values.append((183, "allow_audio_DOWNLOAD", "Download"))
-		self.db.executeBatch(sql, values)
+	def _isSILOnly(self, record):
+		if record != None:
+			return record.Licensor() == "SIL" and record.CoLicensor() == None
+		else:
+			return False
 
 
-	## deprecated
-	def accessGroupSymmetricTest(self):
-		sql = ("SELECT distinct bfc.bible_id, agf.access_group_id, ag.description"
-			" FROM bible_fileset_connections bfc, bible_filesets bf, access_group_filesets agf, access_groups ag"
-			" WHERE bfc.hash_id = bf.hash_id"
-			" AND bf.hash_id = agf.hash_id"
-			" AND agf.access_group_id = ag.id"
-			" ORDER BY bfc.bible_id, ag.description")
-		resultSet = self.db.select(sql, ())
-		with open("test_DBP.txt", "w") as outFile:
-			priorKey = None
-			for (bibleId, accessGroupId, lptsName) in resultSet:
-				key = bibleId
-				if key != priorKey:
-					outFile.write("<DBP_Equivalent>%s</DBP_Equivalent>\n" % (bibleId))
-					priorKey = key
-				outFile.write("\t<%s>-1</%s>\n" % (lptsName, lptsName))
-
-		with open(self.config.filename_lpts_xml, "r") as lpts:
-			results = {}
-			permissions = set()
-			hasLive = False
-			for line in lpts:
-				if "<DBP_Eq" in line:
-					if len(permissions) > 0 and hasLive:
-						results[bibleId] = permissions
-					permissions = set()
-					hasLive = False
-					pattern = re.compile("<DBP_Equivalent[123]?>([A-Z0-9]+)</DBP_Equivalent[123]?>")
-					found = pattern.search(line)
-					if found != None:
-						bibleId = found.group(1)
-				if ">-1<" in line:
-					permissions.add(line.strip())
-				if ">Live<" in line or ">live<" in line:
-					hasLive = True
-		with open("test_LPTS.txt", "w") as outFile:
-			for bibleId in sorted(results.keys()):
-				outFile.write("<DBP_Equivalent>%s</DBP_Equivalent>\n" % (bibleId))
-				for permission in sorted(results[bibleId]):
-					outFile.write("\t%s\n" % (permission))
+	def _isPublicDomain(self, record):
+		if record != None:
+			pattern = re.compile(r"Public Domain", re.IGNORECASE)
+			if pattern.search(record.Licensor()):
+				return True
+			if pattern.search(record.Copyrightc()):
+				if len(record.Copyrightc()) < 20:
+					return True
+				else:
+					print("INFO Possible Public Domain Found in %s: %s" % (record.Reg_StockNumber(), record.Copyrightc()))
+		return False
 
 
 if (__name__ == '__main__'):
 	config = Config()
-	lptsReader = LPTSExtractReader(config)
+	lptsReader = LPTSExtractReader(config.filename_lpts_xml)
 	db = SQLUtility(config)
 	dbOut = SQLBatchExec(config)
 	filesets = UpdateDBPAccessTable(config, db, dbOut, lptsReader)
@@ -180,7 +120,22 @@ if (__name__ == '__main__'):
 	db.close()
 	dbOut.displayStatements()
 	dbOut.displayCounts()
-	#dbOut.execute("test-lpts")
+	dbOut.execute("test-lpts")
+
+"""
+CREATE TEMPORARY TABLE TWOS
+SELECT hash_id FROM access_group_filesets WHERE access_group_id=2;
+
+CREATE TEMPORARY TABLE ONEIGHTY
+SELECT hash_id FROM access_group_filesets WHERE access_group_id=181;
+
+SELECT * from bible_filesets where hash_id in (SELECT hash_id from TWOS where hash_id not in (select hash_id from ONEIGHTY))
+
+SELECT * from bible_filesets where hash_id in (SELECT hash_id from ONEIGHTY where hash_id not in (select hash_id from TWOS))
+
+select * from bible_filesets where hash_id in (select hash_id from access_group_filesets where access_group_id in (191, 193))
+order by id
+"""
 
 
 
