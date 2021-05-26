776d0f20373b2f6ed405df029091d9e4bd42138d
Gary N Griswold <gary@shortsands.com>
2021-05-26 01:05:14 -0400

    Rewrote UnicodeScript as a more useful collection of functions.

--- load/UnicodeScript.py
+++ load/UnicodeScript.py
@@ -10,38 +10,54 @@ from LPTSExtractReader import *
 class UnicodeScript:
 
 
-	def readFile(self, filePath):
+	## Downloads an objects, returns content as an array of lines, but discards first 10 lines
+	def readS3Object(s3Client, s3Bucket, s3Key):
+		response = s3Client.get_object(Bucket=s3Bucket, Key=s3Key)
+		content = response['Body'].read().decode("utf-8")
+		lines = content.split("\n") if content != None else []
+		lines = lines[10:] # discard first 10 lines
+		#print("read", lines)
+		return lines
+
+
+	## Opens a file and returns as an array of strings, but discards first 10 lines
+	def readFile(filePath):
+		file = open(filePath, mode='r', encoding="utf-8")
+		lines = file.readlines()
+		file.close()
+		lines = lines[10:] # discard first 10 lines
+		#print("***** read", len(lines))
+		print(lines)
+		return lines
+
+
+	## Parses XML contents and returns an array of characters
+	def parseXMLStrings(lines):
 		text = []
-		fp = io.open(filePath, mode="r", encoding="utf-8")
-		for i in range(0, 10):
-			discard = fp.readline()
-			#print("discard", discard)
-		for line in fp:
-			#print("read", line)
-			pos = 0
-			while ">" in line[pos:]:
-				pos = line.index(">", pos) + 1
-				if len(line) > (pos + 20):
-					#print("pos", pos)
-					c = line[pos]
-					while c != "<":
-						if c.isalpha():
-							text.append(c)
-						pos += 1
-						if pos >= len(line):
-							c = "<"
-						else:
-							c = line[pos]
-					#print("now", "".join(text))
-				if len(text) > 1000:
-					fp.close
-					#print("Found", "".join(text))
-					return text
-		fp.close
+		inText = False
+		for line in lines:
+			for char in line:
+				if char == "<":
+					inText = False
+				if inText and char.isalpha():
+					text.append(char)
+				if char == ">":
+					inText = True
+		return text
+
+
+	## Converts an array of text string to a array of text chars, which is needed for findScript
+	def textToArray(contents):
+		text = []
+		for line in contents:
+			for char in line:
+				if char.isalpha():
+					text.append(char)
 		return text
 				
-			
-	def findScript(self, text, stockNo, filesetId):
+		
+	## Returns the script code of text based upon results returned by unicodedata	
+	def findScript(text):
 		scriptSet = {}
 		for c in text:
 			#print(c, unicodedata.category(c))
@@ -69,12 +85,11 @@ class UnicodeScript:
 			mostScript = "HAN"
 		if mostScript == "MYANMAR":
 			mostScript = "BURMESE"
-		#if mostScript != None:
-		print("In %s %s %s Found %s at %d pct" % (stockNo, filesetId, ", ".join(message), mostScript, pctMatch))
 		return (mostScript, pctMatch)
 
 
-	def matchScripts(self, fileScript, lptsScript):
+	## Compares the actual script determined from text, and the lpts Script
+	def matchScripts(fileScript, lptsScript):
 		if fileScript == None:
 			return False
 		if lptsScript != None:
@@ -83,6 +98,16 @@ class UnicodeScript:
 		if fileScript == lptsScript:
 			return True
 		return False
+
+
+
+	## This is a convenience method used to check a script using verse_text
+	def checkScripts(db, filesetId, lptsScript):
+		sampleText = UnicodeScript.findVerseText(db, filesetId[:6]) ## can't be in this class
+		textList = UnicodeScript.textToArray(sampleText)
+		actualScript = UnicodeScript.findScript(textList)
+		isMatch = UnicodeScript.matchScripts(actualScript, lptsScript)
+		return isMatch
 
 
 if (__name__ == '__main__'):
@@ -97,6 +122,7 @@ if (__name__ == '__main__'):
 	lptsReader = LPTSExtractReader(config.filename_lpts_xml)
 	db = SQLUtility(config)
 	setTypeCode = 'text_plain'
+	#setTypeCode = 'text_format'
 	sql = ("SELECT distinct c.bible_id, f.id, t.description AS stock_no, f.hash_id"
 		" FROM bible_filesets f, bible_fileset_connections c, bible_fileset_tags t"
 		" WHERE c.hash_id = f.hash_id AND c.hash_id = t.hash_id"
@@ -108,24 +134,22 @@ if (__name__ == '__main__'):
 		writer = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 		writer.writerow(("OK/NOT", "stockNo", "bibleId", "filesetId6", "index", "sample text", "actual script", "pct match", "lpts script", "message"))
 
-		unicodeScript = UnicodeScript()
 		for (bibleId, filesetId, stockNo, hashId) in dbpFilesetList:
 			print(bibleId, filesetId, stockNo)
 			lptsRecord = lptsReader.getByStockNumber(stockNo)
 			textDamIds = lptsRecord.DamIdList("text")
 			textDamIds = lptsRecord.ReduceTextList(textDamIds)
 
-			#bibleId = 'ARBBIB'  
-			#filesetId = 'ARBBIB'
-			#stockNo = 'N1ARB/BIB'
-			#textDamIds = [('ARBBIBN_ET', 1, 'Live')]
 			trySet = set()
 			for (damId, index, status) in textDamIds:
 				if not index in trySet:
 					trySet.add(index)
 					print(" ", damId, index, status)
 					message = None
-			
+					lptsScript = lptsRecord.Orthography(index)
+					if lptsScript == None:
+						message = "No LPTS Script"
+
 					if setTypeCode == "text_format":
 						objKey = "text/%s/%s" % (bibleId, filesetId[:6])
 						print("prefix", objKey)
@@ -136,38 +160,37 @@ if (__name__ == '__main__'):
 							filename = item.get('Key')
 							if filename.endswith(".html") and not filename.endswith("about.html") and not filename.endswith("index.html"):
 								fileKeys.append(filename)
+						print("fileKeys", fileKeys)
 						if len(fileKeys) == 0:
-							print(objKey, "has no files?")
 							message = "DBP has no files"
-						if bibleId == "AMPWBT":
-							print("files", fileKeys)
-
-						pos = 0
-						text = ""
-						while len(text) < 200 and len(fileKeys) > pos:
-							s3Client.download_file("dbp-prod", fileKeys[pos], "./sample.html")
-							text += unicodeScript.readFile("./sample.html")
+						
+						pos = 1 # skip first file
+						lines = []
+						while len("".join(lines)) < 2000 and len(fileKeys) > pos:
+							lines += UnicodeScript.readS3Object(s3Client, "dbp-prod", fileKeys[pos])
 							pos += 1
+						textList = UnicodeScript.parseXMLStrings(lines)
 
 					elif setTypeCode == "text_plain":
-						dbpText = db.selectList("SELECT verse_text FROM bible_verses WHERE hash_id = %s limit 10", (hashId,))
-						#print(dbpText)
-						text = []
-						for dbpVerse in dbpText:
-							for char in dbpVerse:
-								text.append(char)
+						#sql = "SELECT verse_text FROM bible_verses WHERE hash_id IN (SELECT hash_id FROM bible_filesets WHERE id = %s) limit 10"
+						#sampleText = db.selectList(sql, (filesetId,))
+						sampleText = db.selectList("SELECT verse_text FROM bible_verses WHERE hash_id = %s limit 10", (hashId,))
 
-					print("text", "".join(text[:60]))
-					if len(text) == 0 and message == None:
-						message = "No verse text"
+						if len(sampleText) == 0 and message == None:
+							message = "No verse text"
+							textList = []
+						else:
+							textList = UnicodeScript.textToArray(sampleText)
 
-					(fileScript, pctMatch) = unicodeScript.findScript(text, stockNo, damId)
+					print("text", "".join(textList[:60]))
+
+					(fileScript, pctMatch) = UnicodeScript.findScript(textList)
 					print("fileScript", fileScript, pctMatch)
 					lptsScript = lptsRecord.Orthography(index)
 					if lptsScript == None:
 						message = "No LPTS Script"
 
-					isMatch = unicodeScript.matchScripts(fileScript, lptsScript)
+					isMatch = UnicodeScript.matchScripts(fileScript, lptsScript)
 					if isMatch:
 						print("******* MATCH", bibleId, filesetId, fileScript, index)
 						matchAns = "OK"
@@ -177,8 +200,6 @@ if (__name__ == '__main__'):
 						if message == None:
 							message = "Mismatch"
 
-					writer.writerow((matchAns, stockNo, bibleId, damId, index, "".join(text[:20]), fileScript, str(pctMatch) + "%", lptsScript, message))
+					writer.writerow((matchAns, stockNo, bibleId, damId, index, "".join(textList[:20]), fileScript, str(pctMatch) + "%", lptsScript, message))
 
-
-				#sys.exit()
 
