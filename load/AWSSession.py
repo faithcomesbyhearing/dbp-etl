ebe3e14da3484ef489ad4759a1b0b28ea418a0bc
Gary N Griswold <gary@shortsands.com>
2021-06-11 10:57:27 -0400

    Consolidated code in AWSSession to a convenience function.

--- load/AWSSession.py
+++ load/AWSSession.py
@@ -14,68 +14,37 @@ class AWSSession:
 
 	def __init__(self):
 		self.config = Config.shared()
-		session = boto3.Session(profile_name=self.config.s3_aws_profile)
-		if self.config.s3_aws_role == None:
-			self.s3Client = session.client('s3')
-		else:
-			# The calls to AWS STS AssumeRole must be signed with the access key ID
-			# and secret access key of an existing IAM user or by using existing temporary
-			# credentials such as those from another role. (You cannot call AssumeRole
-			# with the access key for the root account.) The credentials can be in
-			# environment variables or in a configuration file and will be discovered
-			# automatically by the boto3.client() function. For more information, see the
-			# Python SDK documentation:
-			# http://boto3.readthedocs.io/en/latest/reference/services/sts.html#client
-
-			# create an STS client object that represents a live connection to the
-			# STS service
-			stsClient = session.client('sts')
-			#print("client", stsClient)
-
-			# Call the assume_role method of the STSConnection object and pass the role
-			# ARN and a role session name.
-			assumedRoleObject = stsClient.assume_role(
-				RoleArn = self.config.s3_aws_role,
-			    RoleSessionName = "AssumeRoleSession1"
-			)
-			#print("role", assumedRoleObject)
-
-			# From the response that contains the assumed role, get the temporary
-			# credentials that can be used to make subsequent API calls
-			credentials = assumedRoleObject['Credentials']
-			#print("credentials", credentials)
-
-			# Use the temporary credentials that AssumeRole returns to make a
-			# connection to Amazon S3
-			#self.s3Client = boto3.resource(
-			self.s3Client = boto3.client(
-			    's3',
-			    aws_access_key_id = credentials['AccessKeyId'],
-			    aws_secret_access_key = credentials['SecretAccessKey'],
-			    aws_session_token = credentials['SessionToken'],
-			)
-			print("Created role based session on role", self.config.s3_aws_role)
+		self.s3Client = self._securityTokenService("s3", "AssumeRoleSession1", "us-west-2")
 
 
 	def elasticTranscoder(self):
-		session = boto3.Session(profile_name=self.config.s3_aws_profile)
+		return self._securityTokenService("elastictranscoder", "AssumeRoleSession2", self.config.video_transcoder_region)
+
+
+	def lambdaInvoker(self):
+		return self._securityTokenService("lambda", "AssumeRoleSession3", self.config.lambda_zip_region)
+
+
+	def _securityTokenService(self, clientType, roleSessionName, regionName):
+		session = boto3.Session(profile_name = self.config.s3_aws_profile, region_name = regionName)
 		if self.config.s3_aws_role == None:
-			transcoderClient = session.client('elastictranscoder', region_name=self.config.video_transcoder_region)
+			client = session.client(clientType)
 		else:
 			stsClient = session.client('sts')
 			assumedRoleObject = stsClient.assume_role(
 				RoleArn = self.config.s3_aws_role,
-			    RoleSessionName = "AssumeRoleSession2"
+			    RoleSessionName = roleSessionName
 			)
 			credentials = assumedRoleObject['Credentials']
-			transcoderClient = boto3.client(
-			    'elastictranscoder',
+			client = boto3.client(
+			    clientType,
 			    aws_access_key_id = credentials['AccessKeyId'],
 			    aws_secret_access_key = credentials['SecretAccessKey'],
 			    aws_session_token = credentials['SessionToken'],
-			    region_name = self.config.video_transcoder_region
-			)
-		return transcoderClient
+			    region_name = regionName
+			)				
+			print("Created role %s based session for %s." % (self.config.s3_aws_role, clientType))
+		return client
 
 
 	def profile(self):
