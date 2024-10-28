e85b5cb860e6005d6bb0d9db831c38820b20feaa
Brad Flood <bflood@keyholesoftware.com>
2024-10-28 09:21:50 -0500

    temporarily ignore insert errors

--- load/SQLBatchExec.py
+++ load/SQLBatchExec.py
@@ -44,7 +44,9 @@ class SQLBatchExec:
 		if len(values) > 0:
 			names = attrNames + pkeyNames
 			valsubs = ["'%s'"] * len(names)
-			sql = "INSERT INTO %s (%s) VALUES (%s);" % (tableName, ", ".join(names), ", ".join(valsubs))
+			# temporary -- remove the error checks. This should only be used in exceptional circumstances
+			# sql = "INSERT INTO %s (%s) VALUES (%s);" % (tableName, ", ".join(names), ", ".join(valsubs))
+			sql = "INSERT IGNORE INTO %s (%s) VALUES (%s);" % (tableName, ", ".join(names), ", ".join(valsubs))
 			for value in values:
 				stmt = sql % value
 				stmt = self.unquoteValues(stmt)
@@ -58,7 +60,9 @@ class SQLBatchExec:
 	def insertSet(self, tableName, pkeyNames, attrNames, values):
 		if len(values) > 0:
 			names = attrNames + pkeyNames
-			stmt = "INSERT INTO %s (%s) VALUES " % (tableName, ", ".join(names))
+			# temporary -- remove the error checks. This should only be used in exceptional circumstances
+			# stmt = "INSERT INTO %s (%s) VALUES " % (tableName, ", ".join(names))
+			stmt = "INSERT IGNORE INTO %s (%s) VALUES " % (tableName, ", ".join(names))
 			self.statements.append(stmt)
 			valsubs = ["'%s'"] * len(names)
 			sql = "(%s)," % (", ".join(valsubs))
